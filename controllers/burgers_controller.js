bdfca25c93573777997e377bd42320168082cf9b
Mel DeJesus <meldejesus@guest-2-00710.dhcp.unc.edu>
2017-05-23 19:58:01 -0400

    Change port variable to lowercase

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,5 +36,15 @@ router.put("/:id", function(req, res) {
   });
 });
 
+router.delete("/:id", function(req, res) {
+  var condition = "id = " +req.params.id;
+
+  project.delete({
+    
+  }
+    completed: 
+  })
+})
+
 // Export routes for server.js to use.
 module.exports = router;
